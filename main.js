a1d1fd137f65e529b954bf13abe6108460295f3f
HazielKaos <romuald.lewandoski@yandex.com>
2023-03-01 13:13:44 +0100

    Edit main.js

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-import Map from 'collections/map'
 import routes from "./src/config/routes";
 import Plasma from "./src/Plasma/Plasma";
 import CustomAuthenticator from "./src/security/CustomAuthenticator";
 import LangManager from "./src/utils/LangManager";
-import apiRoutes from "./src/config/apiRoutes";
-
-var checkStaffTimer, isCheckStaff
 
 $(document).ready(() => {
-    /*window.CORE = new Core()
-    CORE.init()*/
-
     window.PLASMA = new Plasma(config, routes.getRoutes())
     PLASMA.registerAuthenticator(new CustomAuthenticator())
     PLASMA.registerLangManager(LangManager)
     PLASMA.init()
-
 })
